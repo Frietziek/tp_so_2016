0b854ea88de23268eebb22e36481aa82d9fd4732
alfredosantiagobose <alfredosantiagobose@gmail.com>
2016-04-19 02:39:36 -0300

    Creo el proyecto consola

---
+++ consola/src/consola.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : consola.c
+ Author      : losmallocados
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hola soy la consola"); /* prints nombre proceso */
+	return EXIT_SUCCESS;
+}
