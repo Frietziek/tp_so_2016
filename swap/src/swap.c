ec6cf4c5e3a156966a860aa3f47a0bf4d514c5c2
alfredosantiagobose <alfredosantiagobose@gmail.com>
2016-04-19 02:55:20 -0300

    Creo el proyecto cpu y swap

---
+++ swap/src/swap.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : swap.c
+ Author      : losmallocados
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hola soy el swap");
+	return EXIT_SUCCESS;
+}
