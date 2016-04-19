cd9bbc313d495830a980bb4617c597fd894daef1
alfredosantiagobose <alfredosantiagobose@gmail.com>
2016-04-19 02:49:41 -0300

    Creo el proyecto umc

---
+++ umc/src/umc.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : umc.c
+ Author      : losmallocados
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hola soy el umc");
+	return EXIT_SUCCESS;
+}
