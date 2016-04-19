e88a160dfbf0874accd5e765593041425efdf0e2
alfredosantiagobose <alfredosantiagobose@gmail.com>
2016-04-19 02:44:28 -0300

    Creo el proyecto nucleo

---
+++ nucleo/src/nucleo.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : nucleo.c
+ Author      : losmallocados
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hola soy el nucleo"); /* prints proceso */
+	return EXIT_SUCCESS;
+}
