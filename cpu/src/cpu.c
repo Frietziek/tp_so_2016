a5137ec15c20437da9cce474cd08bbfdd84cc444
leoguty91 <leoguty91@gmail.com>
2016-04-20 00:44:03 -0300

    Se agregaron TODOs de los procesos CPU y UMC

--- cpu/src/cpu.c
+++ cpu/src/cpu.c
@@ -12,6 +12,15 @@
 #include <stdlib.h>
 
 int main(void) {
+	// TODO Conectar con el Nucleo (socket client)
+	// TODO Recibir PCB del Nucleo
+	// TODO Incrementar registro Program Counter en PCB
+	// TODO Hacer el parser del indice de codigo
+	// TODO Conectarse al UMC y recibir prox sentencia
+	// TODO Ejecutar operaciones (Primitivas)
+	// TODO Actualizar valores en UMC
+	// TODO Actualizar PC en PCB
+	// TODO Notificar al nucleo que termino el Quantum
 	puts("Hola soy el cpu");
 	return EXIT_SUCCESS;
 }
--- umc/src/umc.c
+++ umc/src/umc.c
@@ -12,6 +12,13 @@
 #include <stdlib.h>
 
 int main(void) {
-	puts("Hola soy el umc");
+	// TODO Leer archivo de Configuracion
+	// TODO Crear bloque de memoria principal
+	// TODO Crear estructuras para programas
+	// TODO Crear Cache TLB
+	// TODO Crear servidor (socket host)
+	// TODO Recibir conexiones de Nucleo y CPUs
+	// TODO Crear hilos por cada conexion
+	puts("Hola soy la umc");
 	return EXIT_SUCCESS;
 }
