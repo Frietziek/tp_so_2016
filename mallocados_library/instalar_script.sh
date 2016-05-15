4373feefb6afad5d80582c71fe68d3b2c220719f
Frietziek <federicohausler@gmail.com>
2016-05-15 13:24:50 -0300

    Se modifica el script para que instale el nuevo  serializaciones.h

--- mallocados_library/instalar_script.sh
+++ mallocados_library/instalar_script.sh
@@ -1,10 +1,14 @@
 #! /bin/bash
 gcc -c -Wall -Werror -fpic comunicaciones.c 
 gcc -c -Wall -Werror -fpic utiles.c 
+gcc -c -Wall -Werror -fpic serializacion.c 
 gcc -shared -o libcomunicaciones.so comunicaciones.o
 gcc -shared -o libutil.so utiles.o
+gcc -shared -o libserializacion.so serializacion.o
 cp libcomunicaciones.so /usr/lib/
 cp libutil.so  /usr/lib/
+cp libserializacion.so  /usr/lib/
 cp comunicaciones.h /usr/include/
 cp utiles.h  /usr/include/
+cp serializacion.h  /usr/include/
 
