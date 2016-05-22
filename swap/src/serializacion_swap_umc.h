b3b8ff651c7d0a5a2b9e4cc1ba2ee0d715f4edff
Frietziek <federicohausler@gmail.com>
2016-05-22 16:51:09 -0300

    Se agregan las deserializaciones de los mensajes recibidos del umc

---
+++ swap/src/serializacion_swap_umc.h
@@ -0,0 +1,42 @@
+/*
+ * serializacion_umc_swap.h
+ *
+ *  Created on: 14/5/2016
+ *      Author: utnso
+ */
+
+#ifndef SERIALIZACION_UMC_H_
+#define SERIALIZACION_UMC_H_
+
+typedef struct {
+	int id_programa;
+	int paginas_requeridas;
+} t_inicio_programa;
+
+typedef struct {
+	int pagina;
+	int offset;
+	int tamanio;
+} t_leer_pagina;
+
+typedef struct {
+	int pagina;
+	int offset;
+	int tamanio;
+	void *buffer;
+} t_escribir_pagina;
+
+typedef struct {
+	int id_programa;
+} t_fin_programa;
+
+
+void deserializar_inicio_programa(void *buffer, t_inicio_programa *inicio_programa);
+
+void deserializar_leer_pagina(void *buffer, t_leer_pagina *leer_pagina);
+
+void deserializar_escribir_pagina(void *buffer, t_escribir_pagina *escribir_pagina);
+
+void deserializar_finalizar_programa(void *buffer, t_fin_programa *finalizar_programa);
+
+#endif /* SERIALIZACION_UMC_H_ */
