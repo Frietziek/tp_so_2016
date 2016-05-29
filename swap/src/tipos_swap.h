818b1700811fcbdddef81af28bf05d6c0f3a9641
Frietziek <federicohausler@gmail.com>
2016-05-29 18:17:57 -0300

    Arreglos chicos, se borran ejemplos dentro del swap y  modularizacion de funciones.

--- swap/src/tipos_swap.h
+++ swap/src/tipos_swap.h
@@ -8,6 +8,29 @@
 #ifndef TIPOS_SWAP_H_
 #define TIPOS_SWAP_H_
 
+// Handshake
+#define MENSAJE_HANDSHAKE 0
+#define REPUESTA_HANDSHAKE 10
+#define ERROR_HANDSHAKE 20
+
+// Perticiones del UMC
+#define MENSAJE_LEER_PAGINA 1
+#define MENSAJE_ESCRIBIR_PAGINA 2
+#define MENSAJE_INICIAR_PROGRAMA 3
+#define MENSAJE_FINALIZAR_PROGRAMA 4
+
+// Respuestas OK
+#define RESPUESTA_LEER_PAGINA 11
+#define RESPUESTA_ESCRIBIR_PAGINA 12
+#define RESPUESTA_INICIALIZAR_PROGRAMA 13
+#define RESPUESTA_FINALIZAR_PROGRAMA 14
+// Respuestas Error
+#define ERROR_LEER_PAGINA 21
+#define ERROR_ESCRIBIR_PAGINA 22
+#define ERROR_INICIALIZAR_PROGRAMA 23
+#define ERROR_FINALIZAR_PROGRAMA 24
+
+
 typedef struct {
 	int puerto_escucha;
 	char *nombre_swap;
