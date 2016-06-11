7d38b42c59f9adedebbabb46145c0f0c0410ea0f
Leonel Gutierrez <leoguty91@gmail.com>
2016-06-10 23:10:21 -0300

    Se sube ejemplo de PCB modificado

--- cpu/src/semaforo_sockets_cpu.h
+++ cpu/src/semaforo_sockets_cpu.h
@@ -10,6 +10,46 @@
 
 #include <semaphore.h>
 #include <commons/log.h> // Libreria de Logs
+#include <commons/collections/list.h> // Libreria para listas
+#include <parser/metadata_program.h>
+
+typedef struct {
+	int pagina;
+	int offset;
+	int size;
+} t_posicion_memoria;
+
+typedef struct {
+	int id;
+	t_posicion_memoria *posicion_memoria;
+} t_variables_stack;
+
+typedef struct {
+	int posicion_retorno;
+	t_posicion_memoria *posicion_variable_retorno;
+	t_variables_stack **variables;
+	t_posicion_memoria **argumentos;
+} t_indice_stack;
+
+typedef struct {
+	int pid;
+	int pc;
+	int cant_paginas_codigo_stack;
+	int estado;
+	int stack_size_maximo;
+	int stack_position;
+	//t_size etiquetas_size; // Tamaño del mapa serializado de etiquetas
+	char* etiquetas;
+	t_size instrucciones_size;
+	t_intructions *instrucciones_serializadas;
+	int stack_size_actual;
+	t_indice_stack **indice_stack;
+} t_pcb;
+
+typedef struct {
+	int quantum;
+	t_pcb *pcb;
+} t_pcb_quantum;
 
 // Semaforos para continuar proceso y finalizar
 sem_t s_pagina;
@@ -25,5 +65,8 @@ int tamanio_pagina;
 
 // Logger
 t_log *logger_manager;
+
+// PCB - Quantum
+t_pcb_quantum *pcb_quantum;
 
 #endif /* SEMAFORO_SOCKETS_CPU_H_ */
