87caf82833af45788a8a274adcdcdf0919e6d7e2
Sergio Nicosia <sergiodnicosia@gmail.com>
2016-07-19 20:04:59 -0300

    Fix menor en el ultimo commit

--- cpu/src/cpu.c
+++ cpu/src/cpu.c
@@ -492,7 +492,7 @@ void respuesta_leer_compartida(void *buffer) {
 	t_variable_completa *compartida = malloc(sizeof(t_variable_completa));
 	deserializar_variable_completa(buffer, compartida);
 	valor_pagina = realloc(valor_pagina, sizeof(int));
-	valor_pagina = compartida->valor;
+	memcpy(valor_pagina, compartida->valor, sizeof(int));
 	free(compartida);
 	sem_post(&s_variable_compartida);
 }
